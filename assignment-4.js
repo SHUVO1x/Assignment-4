0d1821b442b1023becc336d21e5eef9197f3d3c1
SHUVO1x <mohammadkhayearalam@gmail.com>
2025-11-01 02:14:09 +0600

    1st problelm

---
+++ assignment-4.js
@@ -0,0 +1,11 @@
+// problem-1
+function totalFine( fare ) {
+          if(!(typeof fare === 'number') || fare<1 ){
+            return "Invalid";
+          }
+          return fare+((fare * .2) + 30 )
+}
+
+// problem-2
+
+
